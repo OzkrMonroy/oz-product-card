bdfb2e724f8eb4c4a392410f3bac490441db4e68
Oscar Monroy <oscarmonroy657@gmail.com>
2022-07-01 13:35:59 -0600

    Version 1.0.0-rc is ready.

--- src/components/ProductTitle.tsx
+++ src/components/ProductTitle.tsx
@@ -5,6 +5,7 @@ import styles from '../styles/styles.module.css';
 
 export const ProductTitle = ({ name, className, style }: ProductTitleProps) => {
   const { product } = useContext(ProductContext);
+
   return (
     <span className={`${styles.productDescription} ${className}`} style={style}>
       {name ? name : product.name}
